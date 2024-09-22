70578f6b630eadf71727d8159353204d6b946fec
John <13438710+ekalnhoj@users.noreply.github.com>
2024-09-22 15:10:06 -0500

    Packaging Libraries
    Didn't realize this wasn't automatic :(

---
+++ Locales/Locale-enUS.lua
@@ -0,0 +1,27 @@
+﻿-- HandyNotes
+-- enUS and enGB Localization file
+
+local L = LibStub("AceLocale-3.0"):NewLocale("MissedCooldownTracker", "enUS", true)
+
+
+L["MissedCooldownTracker"] = true
+
+L["Enable MissedCooldownTracker"] = true
+L["Enable or disable MissedCooldownTracker"] = true
+
+L["Overall settings"] = true
+L["Overall settings that affect every database"] = true
+L["These settings control the execution of CooldownTracker globally."] = true
+L["Learning Mode"] = true
+L["Whether CooldownTracker checks for new spell IDs on cooldown."] = true
+L["L["Min CD length tracked"] = true
+L["The minimum cooldown length (in seconds) to track."] = true
+L["Max CD length tracked"] = true
+L["The maximum cooldown length (in seconds) to track."] = true
+L["MCDT Update period"] = true
+L["Interval at which CooldownTracker checks spell cooldowns."] = true
+
+L["Spell list"] = true
+L["List of spells this character has access to."] = true
+
+-- vim: ts=4 noexpandtab
